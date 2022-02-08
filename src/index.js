7605907492946cb7ba3cdc29e1d06970ca57119f
drcyberx <drcyberx@gmail.com>
2022-02-08 12:13:03 +0530

    initilizing app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import faker from "@faker-js/faker";
+
+let products = "";
+
+for (let i = 0; i < 3; i++) {
+  const name = faker.commerce.productName();
+  products += `<div>${name}</div>`;
+}
+
+console.log(products);
+
+document.querySelector("#dev-products").innerHTML = products;
